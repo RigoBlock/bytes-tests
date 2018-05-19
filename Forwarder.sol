2f6165fe3ce57d60b3ca0165ec30781492a01c3c
escursionista <gabriele@rigoinvestment.com>
2018-05-19 23:12:59 +0200

    add raw forwarder

--- Forwarder.sol
+++ Forwarder.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.4.23;
 pragma experimental "v0.5.0";
 
-/// @title Forwarder - Forward abi-encoded transactions.
+/// @title Casper - Interface of the casper contract.
 /// @author Gabriele Rigo - <gab@rigoblock.com>
 /// @notice ptublic casper functions expressed as external
 contract Forwarder {
+    
+    address public exchange;
 
+    function() external {
+        exchange.delegatecall(msg.data);
+    }
+    
+    function setExchange(address _exchange)
+        public
+    {
+        exchange = _exchange;
+    }
+    
+    function getExchange()
+        external view
+        returns (address)
+    {
+        return exchange;
+    }
+    
     function forwardCall(
         address target,
         bytes data)
@@ -28,7 +47,31 @@ contract Forwarder {
         bytes data)
         public
     {
-        bytes4 method = 0xbc61394a; //signature of 0x fillOrder() function
+        bytes4 method = 0xbc61394a;
         target.delegatecall(method, data);
     }
+
+/*
+    function assembleCall(
+        address target,
+        bytes data)
+        public
+    {
+        bytes4 sig = 0xbc61394a;
+        uint256 argsSize = 1 * 32;
+        // sig + arguments size
+        uint256 dataSize = 4 + argsSize;
+
+        bytes memory m_data = new bytes(dataSize);
+
+        assembly {
+            // Add the signature first to memory
+            mstore(add(m_data, 0x20), sig)
+            // Add the parameters
+            mstore(add(m_data, 0x24), data)
+        }
+
+        target.delegatecall(sig, m_data);
+    }
+*/
 }
